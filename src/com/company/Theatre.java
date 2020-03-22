5d2c0ccafc9c95672d5e81d497465b7a6becc46a
mihino89 <martin.mihalovic98@gmail.com>
2020-03-22 14:45:16 +0100

    Before gitflow

--- src/com/company/Theatre.java
+++ src/com/company/Theatre.java
@@ -7,7 +7,7 @@ import java.util.Collections;
 
 public class Theatre {
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public Theatre(String theatreName, int numRows, int seatPerRow) {
         this.theatreName = theatreName;
@@ -54,7 +54,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat>{
+    public class Seat implements Comparable<Seat>{
         private final String seatNumber;
         private boolean reserved = false;
 
